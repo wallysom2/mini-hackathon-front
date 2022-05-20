2097c4dc3f529ca6f906220fae9210f622c517c2
Wallyson <wallysom2@gmail.com>
2022-05-20 19:35:19 -0300

    new branch

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Home from './Home';
+import Questions from './Questions';
+
 function App() {
   return(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/questions" element={<Questions />} />
+
       </Routes>
     </BrowserRouter>)
 }
