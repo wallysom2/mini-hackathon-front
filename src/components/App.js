866a943c4a364e34afa6304a66d1aa98cac8f097
Matheus-daSilva <matheus2001silva@live.com>
2022-05-20 21:41:38 -0300

    add: Home.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
   return(
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,2 +1,20 @@
 import styled from 'styled-components';
 
+function Header() {
+    return (
+        <Div>
+            <img src="https://w7.pngwing.com/pngs/612/406/png-transparent-kathmandu-athens-nepali-language-greece-miscellaneous-logo-structure.png" alt="paternon"></img>
+            <P>Faça uma pergunta</P>
+        </Div>
+    )
+}
+
+export default Header;
+
+const Div = styled.header`
+display: flex;
+`
+
+const P = styled.p`
+
+`
